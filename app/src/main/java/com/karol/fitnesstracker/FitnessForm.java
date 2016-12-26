f9a7ab339112b57b0add3a9695243b9f903bf5ca
karolzdebel <kzdebel@mail.uoguelph.ca>
2016-12-26 05:36:10 -0500

    Progressing FitnessForm

---
+++ app/src/main/java/com/karol/fitnesstracker/FitnessForm.java
@@ -0,0 +1,33 @@
+package com.karol.fitnesstracker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.R.layout.*;
+
+/**
+ * Created by K on 2016-12-26.
+ */
+
+public class FitnessForm extends AppCompatActivity {
+
+    private Spinner activityLevelSpinner;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fitness_form);
+
+        createSpinner();
+    }
+
+    private void createSpinner(){
+        this.activityLevelSpinner = (Spinner) findViewById(R.id.fitness_form_activity_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.fitness_form_activity, android.R.layout.simple_spinner_dropdown_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        activityLevelSpinner.setAdapter(adapter);
+    }
+}
