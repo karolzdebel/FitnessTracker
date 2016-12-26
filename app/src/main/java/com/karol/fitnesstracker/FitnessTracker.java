f494b3345d5198df080c582479d97ec317d4cbeb
karolzdebel <kzdebel@mail.uoguelph.ca>
2016-12-25 23:35:20 -0500

    Adding files for FitnessTracker for version control

---
+++ app/src/main/java/com/karol/fitnesstracker/FitnessTracker.java
@@ -0,0 +1,13 @@
+package com.karol.fitnesstracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FitnessTracker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fitness_tracker);
+    }
+}
