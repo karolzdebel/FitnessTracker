8b102daf068f011cc5e25177f9c329a137f0e050
karolzdebel <kzdebel@mail.uoguelph.ca>
2016-12-26 01:34:11 -0500

    Adding files for FitnessTracker for version control

--- app/src/main/java/com/karol/fitnesstracker/FitnessTracker.java
+++ app/src/main/java/com/karol/fitnesstracker/FitnessTracker.java
@@ -2,12 +2,31 @@ package com.karol.fitnesstracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class FitnessTracker extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fitness_tracker);
+
+        Button buttonRegister = (Button) findViewById(R.id.change_button_register);
+        Button buttonLogin = (Button) findViewById(R.id.change_button_login);
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.register_layout);
+            }
+        });
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.login_layout);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/karol/fitnesstracker/Register.java
@@ -0,0 +1,17 @@
+package com.karol.fitnesstracker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by K on 2016-12-25.
+ */
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_layout);
+    }
+}
