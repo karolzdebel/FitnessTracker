8468c284a315e007d019aacdc941099f2c6180c8
karolzdebel <kzdebel@mail.uoguelph.ca>
2016-12-31 11:41:13 -0500

    Fixed margins and added titles to login and register activities.

--- app/src/main/java/com/karol/fitnesstracker/Register.java
+++ app/src/main/java/com/karol/fitnesstracker/Register.java
@@ -1,7 +1,10 @@
 package com.karol.fitnesstracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by K on 2016-12-25.
@@ -13,5 +16,17 @@ public class Register extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_layout);
+
+        Button submitButton = (Button) findViewById(R.id.register_submit_button);
+
+        final Intent goToLogin = new Intent(this, Login.class);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(goToLogin);
+            }
+        });
+
     }
 }
