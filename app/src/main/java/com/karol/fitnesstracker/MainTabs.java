c894a0d94a9a1266be6d21bf13fed7b7f26a5472
karolzdebel <kzdebel@mail.uoguelph.ca>
2016-12-30 21:26:45 -0500

    Tab are now functioning

--- app/src/main/java/com/karol/fitnesstracker/MainTabs.java
+++ app/src/main/java/com/karol/fitnesstracker/MainTabs.java
@@ -1,21 +1,17 @@
 package com.karol.fitnesstracker;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.TextView;
 
 public class MainTabs extends AppCompatActivity {
@@ -40,8 +36,6 @@ public class MainTabs extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_tabs);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
@@ -50,16 +44,8 @@ public class MainTabs extends AppCompatActivity {
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.main_tabs_tablayout);
+        tabLayout.setupWithViewPager(mViewPager);
     }
 
 
@@ -140,18 +126,22 @@ public class MainTabs extends AppCompatActivity {
         @Override
         public int getCount() {
             // Show 3 total pages.
-            return 3;
+            return 5;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return "Home";
                 case 1:
-                    return "SECTION 2";
+                    return "Workout";
                 case 2:
-                    return "SECTION 3";
+                    return "Progress";
+                case 3:
+                    return "Nutrition";
+                case 4:
+                    return "Settings";
             }
             return null;
         }
