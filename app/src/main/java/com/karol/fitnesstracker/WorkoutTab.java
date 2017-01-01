7e9643c9f161e1831992dbcd061b167dacb6d9b4
karolzdebel <kzdebel@mail.uoguelph.ca>
2017-01-01 18:30:50 -0500

    Created layout for the create workout page

--- app/src/main/java/com/karol/fitnesstracker/WorkoutTab.java
+++ app/src/main/java/com/karol/fitnesstracker/WorkoutTab.java
@@ -1,11 +1,12 @@
 package com.karol.fitnesstracker;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by K on 2016-12-27.
@@ -29,6 +30,18 @@ public class WorkoutTab extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View workoutView = inflater.inflate(R.layout.workout_layout,container,false);
+
+        //Intent createWorkoutIntent = new Intent(this,)
+
+
+        Button createWorkoutButton = (Button) workoutView.findViewById(R.id.workout_tab_create_workout_button);
+        createWorkoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
         return workoutView;
     }
 }
